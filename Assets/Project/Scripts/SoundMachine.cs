92a5e584cd69be144de8732f303476613fc82699
Yasaman Farazan <ya7ya7ya2@yahoo.com>
2020-06-17 08:51:41 +0200

    Project init and some implementations

---
+++ Assets/Project/Scripts/SoundMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundMachine : MonoBehaviour
+{
+    [SerializeField] private Gradient gradient;
+    [SerializeField] private Image backgroundColor;
+    [SerializeField] private TextMeshProUGUI daytimeText;
+    [Space] 
+    [SerializeField] private SoundData[] soundData;
+
+    private void OnEnable()
+    {
+        backgroundColor.color = gradient.Evaluate(0);
+    }
+
+    public void OnSliderValueChanged(Single value)
+    {
+        backgroundColor.color = gradient.Evaluate(value / 100);
+    }
+}
+
+[System.Serializable]
+struct SoundData
+{
+    public string dayTimeName;
+    public AudioClip clip;
+    public float startTime;
+    public float endTime;
+}
